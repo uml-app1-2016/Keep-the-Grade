be94238a85e0b45ae71a0be238f4755177283c6b
Ashley Hale <ashhale@gmail.com>
2016-11-15 15:51:33 -0500

    Added a ClassActivity

--- app/src/main/java/edu/uml/android/keepthegrade/MainActivity.java
+++ app/src/main/java/edu/uml/android/keepthegrade/MainActivity.java
@@ -8,40 +8,4 @@ import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    CategoryAdapter adapter = null;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-       /* WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://chart.apis.google.com/chart?\n" +
-                "chs=200x200\n" +
-                "&chdlp=b\n" +
-                "&chtt=Uberman\n" +
-                "&chdl=Asleep|Awake\n" +
-                "&chd=t:1,11,1,11,1,11,1,11,1,11,1,11\n" +
-                "&cht=p\n" +
-                "&chco=586F8E,7D858F,586F8E,7D858F,586F8E,7D858F,586F8E,7D858F,586F8E,7D858F,586F8E,7D858F");*/
-
-        // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        // Create an adapter that knows which fragment should be shown on each page
-        adapter = new CategoryAdapter(this, getSupportFragmentManager());
-
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
-
-        // Find the tab layout that shows the tabs
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        // Connect the tab layout with the view pager. This will
-        //   1. Update the tab layout when the view pager is swiped
-        //   2. Update the view pager when a tab is selected
-        //   3. Set the tab layout's tab names with the view pager's adapter's titles
-        //      by calling onPageTitle()
-        tabLayout.setupWithViewPager(viewPager);
-    }
 }
