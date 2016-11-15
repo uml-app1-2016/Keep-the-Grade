a7dd21e2364f09288aca33d0d7a16dadff682b52
timLarocque <timothy_larocque@student.uml.edu>
2016-11-15 12:02:30 -0500

    working on class activity

---
+++ app/src/main/java/edu/uml/android/keepthegrade/ClassActivity.java
@@ -0,0 +1,16 @@
+package edu.uml.android.keepthegrade;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tim on 11/15/2016.
+ */
+
+public class ClassActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class);
+    }
+}
